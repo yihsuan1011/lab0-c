31e7c2d27cc284f9f71954d8626c1ba965590770
yihsuan1011 <t29922154@gmail.com>
2023-03-04 22:09:30 +0800

    Implement q_remove_tail, improve q_remove_head

--- queue.c
+++ queue.c
@@ -67,17 +67,15 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head)
+    if (!head || head->next == head)
         return NULL;
     element_t *target = list_first_entry(head, element_t, list);
-    if (&target->list == head)
-        return NULL;
     if (sp) {
         if (strlen(target->value) > (bufsize - 1))
             strncpy(sp, target->value, bufsize - 1);
         else
             sp = strdup(target->value);
-        strcat(sp, "/0");
+        strcat(sp, "\0");
     }
     list_del(&target->list);
     return target;
@@ -86,7 +84,18 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->next == head)
+        return NULL;
+    element_t *target = list_last_entry(head, element_t, list);
+    if (sp) {
+        if (strlen(target->value) > (bufsize - 1))
+            strncpy(sp, target->value, bufsize - 1);
+        else
+            sp = strdup(target->value);
+        strcat(sp, "\0");
+    }
+    list_del(&target->list);
+    return target;
 }
 
 /* Return number of elements in queue */
