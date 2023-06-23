6ec41fe7278f5f9ef63a48407bf7722553fd1af8
yihsuan1011 <t29922154@gmail.com>
2023-06-23 16:39:38 +0800

    Update q_size to handle empty list

--- queue.c
+++ queue.c
@@ -90,7 +90,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    if (!head)
+    if (!head || list_empty(head))
         return 0;
 
     int len = 0;
